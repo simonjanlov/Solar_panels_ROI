aa9b3eb8ca7331797bb2f04f9bdf60cfff4a1b3e
Simon Janlöv <simon.janlov@gmail.com>
2023-10-25 15:18:55 +0200

    added the not_used_in_app folder with calculation_of_swedish_avg.py file

---
+++ python_scripts/not_used_in_app/calculation_of_swedish_avg.py
@@ -0,0 +1,7 @@
+# genomsnitt för Sveriges genomsnittliga elpriser år 2013 - 2022
+
+sverige_tot = [56.4, 58.2, 59.4, 61.4, 64.2, 67.1, 68.9, 69.7, 69.8, 72.9]
+
+totalt_genomsnitt_SE = sum(sverige_tot) / len(sverige_tot)
+
+print(totalt_genomsnitt_SE)
