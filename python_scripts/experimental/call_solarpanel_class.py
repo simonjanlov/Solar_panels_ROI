9affa4962630fa31c261a54b05215a53129135dd
Simon Janlöv <simon.janlov@gmail.com>
2023-10-26 11:30:08 +0200

    changed the default number of years in the SolarPanelSystem class back to 30 years

--- python_scripts/experimental/call_solarpanel_class.py
+++ python_scripts/experimental/call_solarpanel_class.py
@@ -1,4 +1,5 @@
 import sys
+import pandas as pd
 
 sys.path.append(r'C:\projekt\slutprojekt\final_project\python_scripts')
 
@@ -10,12 +11,19 @@ insolation = float(input("Input your location's average yearly insolation: "))
 
 my_system = SolarPanelSystem(price, system_effect, insolation)
     
-mockup_electricity_prices = [1.02, 0.52, 0.49, 1.03, 0.89,
+mockup_electricity_prices = [1.02, 0.52, 0.49, 1.03, 0.89, 
                              0.98, 0.59, 0.42, 0.38, 0.36, 
                              0.48, 0.66, 0.94, 0.82, 0.39, 
-                             0.31, 0.84, 0.57, 0.84, 0.46]
+                             0.31, 0.84, 0.57, 0.84, 0.46, 
+                             0.45, 0.34, 0.66, 0.64, 0.97, 
+                             0.39, 0.88, 0.35, 0.8, 0.33]
+
+electricity_prices_df = pd.read_csv(r'C:\projekt\slutprojekt\final_project\data\predicted_prices_withzones.csv')
+predicted_prices_SE = list(electricity_prices_df['zone1'])
+# print(predicted_prices_SE)
     
 # print(my_system.calc_yearly_electricity_output())
 # print(my_system.electricity_cost_saved_per_year(0.98))
 
-print(my_system.profitability_over_time(mockup_electricity_prices))
+# print(my_system.profitability_over_time(mockup_electricity_prices))
+print(my_system.profitability_over_time(predicted_prices_SE))
