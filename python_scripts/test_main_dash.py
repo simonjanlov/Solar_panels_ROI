404991434fd8a07c142c9cb9edd307212527b860
Simon Janlöv <simon.janlov@gmail.com>
2023-10-27 14:29:24 +0200

    worked on test_main_dash.py and got the gauge number updating interactively

--- python_scripts/test_main_dash.py
+++ python_scripts/test_main_dash.py
@@ -5,9 +5,11 @@ import plotly.graph_objects as go
 import pandas as pd
 from dash_bootstrap_templates import load_figure_template
 
-# Import class and function for Solar panel
+# Import class and functions
 from electricity_output_calc import SolarPanelSystem
 from find_tilt_and_direction_value import find_tilt_and_direction_value
+from calc_years_until_breakeven import calc_years_until_breakeven
+
 
 # Import the data for cities and solar packages
 from data_dicts import packages_dict, cities_dict, years_list
@@ -20,7 +22,7 @@ load_figure_template("vapor")
 app = Dash(__name__, external_stylesheets=[dbc.themes.VAPOR])
 
 # Load the price prognoses data
-price_prognoses_data = pd.read_csv(r'el_priser_data\data\predicted_prices_withzones.csv')
+price_prognoses_data = pd.read_csv(r'data\predicted_prices_withzones.csv')
 
 prognoses_fig = px.line(price_prognoses_data, x='Year', y='Predicted kWh price', title='Price Prognoses')
 prognoses_fig = px.line(price_prognoses_data, x='Year', y=['Predicted kWh price', 'zone1', 'zone2', 'zone3', 'zone4'], title='Price Prognoses')
@@ -39,7 +41,7 @@ fig = go.Figure(go.Indicator(
 city_dropdown = dcc.Dropdown(
     id='city-dropdown',
     options=['Malmö', 'Stockholm', 'Sundsvall', 'Luleå'],
-    value='Luleå',
+    value='Malmö',
     className='mb-3',
     style={'color': 'black', 'width': '100%'}  # Apply Bootstrap classes
 )
@@ -47,7 +49,7 @@ city_dropdown = dcc.Dropdown(
 package_dropdown = dcc.Dropdown(
     id='package-dropdown',
     options=['Package 1 (12 solar panels)', 'Package 2 (25 solar panels)', 'Package 3 (35 solar panels)', 'Package 4 (45 solar panels)'],
-    value='Package 1 (12 solar panels)',
+    value='Package 2 (25 solar panels)',
     className='mb-3',
     style={'color': 'black', 'width': '100%'}  # Apply Bootstrap classes
 )
@@ -55,14 +57,14 @@ package_dropdown = dcc.Dropdown(
 angle_dropdown = dcc.Dropdown(
     id='angle-dropdown',
     options=['0', '10', '20', '30', '40', '50', '60', '70', '80', '90'],
-    value='20',
+    value='40',
     className='mb-3',
     style={'color': 'black', 'width': '100%'}  # Apply Bootstrap classes
 )
 direction_dropdown = dcc.Dropdown(
     id='direction-dropdown',
     options=['270 V', '225 SV', '180 S', '135 SO', '90 E'],
-    value='225 SV',
+    value='270 V',
     className='mb-3',
     style={'color': 'black', 'width': '100%'}  # Apply Bootstrap classes
 )
@@ -108,22 +110,38 @@ main_fig = px.bar(years_profit_df, x='Years', y='Profit', title='Return of Inves
 
 # Create a callback for updating the chart
 @app.callback(
-    Output('line-chart', 'figure'),
+    [Output('line-chart', 'figure'),
+     Output('circle-with-number', 'figure')],
     [Input('city-dropdown', 'value'),
      Input('package-dropdown', 'value'),
      Input('angle-dropdown', 'value'),
      Input('direction-dropdown', 'value')]
 )
 def update_output(selected_city, selected_package, selected_angle, selected_direction):
+    
+    # update the bar chart
     tilt_and_direction = find_tilt_and_direction_value(int(selected_angle), selected_direction)
+    
     my_system = SolarPanelSystem(system_cost=packages_dict[selected_package]['system_cost'],
                                  system_effect_kWp=packages_dict[selected_package]['system_effect'],
                                  insolation=cities_dict[selected_city]['insolation'],
                                  tilt_and_direction=tilt_and_direction)
+    
     profit_values = my_system.profitability_over_time(cities_dict[selected_city]['predicted_prices'])
     years_profit_df = pd.DataFrame({'Years': years_list, 'Profit': profit_values})
     main_fig = px.bar(years_profit_df, x='Years', y='Profit', title='Return of Investment')
-    return main_fig
+
+    # update the numerical figure
+    fig = go.Figure(go.Indicator(
+    mode="gauge+number",
+    value=calc_years_until_breakeven(years_list, profit_values),
+    domain={'x': [0, 1], 'y': [0, 1],
+            'row': 0, 'column': 0},
+    title={'text': "No of years "},
+    gauge={'bar': {'color': "blue"}  # Change the color here
+    }))
+    
+    return main_fig, fig
 
 # Define the app layout
 # ... (your existing code above)
@@ -151,7 +169,7 @@ app.layout = dbc.Container(fluid=True, children=[
                         dbc.Row(
                             [
                                 dbc.Col(dcc.Graph(id='line-chart', figure=main_fig), lg=6),
-                                dbc.Col(dcc.Graph(figure=fig), lg=6),
+                                dbc.Col(dcc.Graph(id='circle-with-number', figure=fig), lg=6),
                                 
                             ],
                             className="mt-4",
