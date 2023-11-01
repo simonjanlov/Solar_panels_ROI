ac4fd7059aa8df920f165f27b82c451b0d19593b
Simon Janlöv <simon.janlov@gmail.com>
2023-11-01 15:29:56 +0100

    added config2.py instead, and added to gitignore

--- config.py
+++
@@ -1,6 +0,0 @@
-OpenWeather_API_KEY = 'YOUR API KEY GOES HERE'
-
-price_prognosis_csvpath = r'data\predicted_prices_withzones.csv' # test_main_dash2.py
-electicity_generation_by_source_csvpath = r'data\Electricity generation by source - Sweden.csv' # test_main_dash2.py
-tilt_and_direction_table_csvpath = r'data\tilt_and_direction_table.csv' # find_tilt_and_direction_value.py
-predicted_prices_withzones_csvpath = r'data\predicted_prices_withzones.csv' # data_dicts.py
