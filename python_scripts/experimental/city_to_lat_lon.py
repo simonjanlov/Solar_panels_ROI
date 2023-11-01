a96cec276da100ef199f554e7478e21740670169
Henry Apler <henry.apler@gmail.com>
2023-11-01 10:30:54 +0100

    made a function that takes city name and return lon lat

---
+++ python_scripts/experimental/city_to_lat_lon.py
@@ -0,0 +1,22 @@
+import requests
+import pandas as pd
+
+def city_to_lat_lon(city):
+
+    api_key = 'b75a85a55f630b5c79dc87feece0d6bd'
+    api_url = f'http://api.openweathermap.org/geo/1.0/direct?q={city},&limit=1&appid={api_key}'
+    response = requests.get(api_url)
+    data = response.json()
+    df = pd.json_normalize(data)
+    lat = df["lat"][0]
+    lon = df["lon"][0]
+    return lon,lat
+# if response.status_code == requests.codes.ok:
+#     print(response.text)
+# else:
+#     print("Error:", response.status_code, response.text)
+        
+
+if __name__ == "__main__":
+    print(city_to_lat_lon('Göteborg'))
+
