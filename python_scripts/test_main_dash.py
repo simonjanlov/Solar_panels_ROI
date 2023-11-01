a220c082cd1d4e6f5fe6db41d19bebd696cb3e80
rameez1822534 <ch_rameez89@hotmail.com>
2023-11-01 10:21:36 +0100

    server added

--- python_scripts/test_main_dash.py
+++ python_scripts/test_main_dash.py
@@ -20,7 +20,7 @@ load_figure_template("superhero")
 
 # Create a Dash app with Bootstrap "superhero" theme
 app = Dash(__name__, external_stylesheets=[dbc.themes.SUPERHERO])
-
+server = app.server
 # Load the price prognoses data
 price_prognoses_data = pd.read_csv(r'final_project\data\predicted_prices_withzones.csv')
 data = pd.read_csv(r'final_project\data\Electricity generation by source - Sweden.csv')
