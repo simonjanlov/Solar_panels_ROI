97067a13821b87399cd12a0f35345ae8ae887c68
rameez1822534 <ch_rameez89@hotmail.com>
2023-11-02 09:34:11 +0100

    created new config file

---
+++ config2.py
@@ -0,0 +1,5 @@
+OpenWeather_API_KEY = '885cc30c15c4aa8545d655750bce81ab'
+price_prognosis_csvpath = r'final_project\data\predicted_prices_withzones.csv'
+electicity_generation_by_source_csvpath = r'final_project\data\Electricity generation by source - Sweden.csv'
+tilt_and_direction_table_csvpath = r'final_project\data\tilt_and_direction_table.csv'
+predicted_prices_withzones_csvpath = r'final_project\data\predicted_prices_withzones.csv'
